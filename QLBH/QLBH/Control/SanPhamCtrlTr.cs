ad76847e4d09113c31bbb6c049f867040de33716
tuanpanda <dxtuan1997@gmail.com>
2018-05-11 10:53:25 +0700

    sanphamctr

--- QLBH/QLBH/Control/SanPhamCtrlTr.cs
+++ QLBH/QLBH/Control/SanPhamCtrlTr.cs
@@ -11,6 +11,26 @@ namespace QLBH.Control
 {
     class SanPhamCtrlTr
     {
-        
+        public static DataTable GetListSanPham()
+        {
+            return SanPhamModTr.GetListSanPham();
+
+        }
+        public static int InsertSanPham(SanPhamObjTr ctd)
+        {
+            return SanPhamModTr.InsertSanPham(ctd);
+        }
+
+        public static int UpdateSanPham(SanPhamObjTr ctd)
+        {
+            return SanPhamModTr.UpdateSanPham(ctd);
+
+        }
+
+        public static int DeleteSanPham(SanPhamObjTr ctd)
+        {
+            return SanPhamModTr.DeleteSanPham(ctd);
+
+        }
     }
 }
