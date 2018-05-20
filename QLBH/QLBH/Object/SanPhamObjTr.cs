361f63ec4eabfc6914e1c827c09729f7ae732fc6
OneyCw <huyhttt14@gmail.com>
2018-05-20 21:56:21 +0700

    Obj oki rồi :+1:

---
+++ QLBH/QLBH/Object/SanPhamObjTr.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLBH.Object
+{
+    class SanPhamObjTr
+    {
+        public string MaSP{ get; set; }
+        public string TenSP { get; set; }
+        public string HangSX { get; set; }
+        public string DVT { get; set; }
+        public float GiaNhap{ get; set; }
+        public float GiaBan { get; set; }
+        public int SoLuong { get; set; }    
+        public string MaQuay { get; set; }
+    }
+}
